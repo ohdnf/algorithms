99dbd1c95a258f11e72ba13747d2f29227833e97
ohdnf <jupyohong7@gmail.com>
2020-02-12 14:20:47 +0900

    200212 ssafy

--- acmicpc/10158_개미.py
+++ acmicpc/10158_개미.py
@@ -4,19 +4,14 @@ input = lambda: sys.stdin.readline()
 w, h = map(int, input().split())
 p, q = map(int, input().split())
 t = int(input())
-x, y = 1, 1
-p += x
-q += y
-# 천장
-if q == h:
-    y = -1
-# 바닥
-if q == 0:
-    y = 1
-# 왼쪽
-if p == 0:
-    x = 1
-# 오른쪽
-if p == w:
-    x = -1
+x = t // w
+y = t // h
+if x % 2:
+    pass
+else:
+    p = w-p
+if y % 2:
+    pass
+else:
+    q = h-q
 print(p, q)
