e2aaf26123d73ce2b2d5798fc49c7efdf61bbf57
ohdnf <jupyohong7@gmail.com>
2020-03-08 23:34:52 +0900

    Add swea 5658

--- swea/mock_exam/5658/5658_treasure.py
+++ swea/mock_exam/5658/5658_treasure.py
@@ -3,4 +3,13 @@ sys.stdin = open('input.txt')
 
 t = int(input())
 for test_case in range(1, t+1):
-    
+    n, k = map(int, input().split())
+    hexa = input()
+    numbers = set()
+    interval = n // 4
+    for _ in range(interval):
+        for idx in range(0, n, interval):
+            numbers.add(int(hexa[idx:idx+interval], 16))
+        hexa = hexa[-1] + hexa[:-1]
+    numbers = sorted(list(numbers), reverse=True)
+    print('#{} {}'.format(test_case, numbers[k-1]))
