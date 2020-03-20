421d5b232a8c0cb11006d5a89d7b5749f6578988
ohdnf <jupyohong7@gmail.com>
2020-03-20 23:48:28 +0900

    Update algorithm(on-going)

--- swea/mock_exam/5656/5656_brick_breaking.py
+++ swea/mock_exam/5656/5656_brick_breaking.py
@@ -1,38 +1,53 @@
 import sys
 sys.stdin = open('input.txt')
 
-def check_brick(x, y):
-    global w, h
-    number = bricks[x][y]
-    to_be_destroyed[x][y] = True
-    if number > 1:
+def arrange():
+    return
+
+def shoot(bricks):
+    q = list()
+    while q:
+        x, y = q.pop(0)
+        number = bricks[x][y]
+        bricks[x][y] = 0
         for dx, dy in ((0, 1), (0, -1), (1, 0), (-1, 0)):
             for dist in range(1, number):
                 nx = x + dx*dist
                 ny = y + dy*dist
-                if 0<=nx<h and 0<=ny<w and not to_be_destroyed[nx][ny]:
-                    if bricks[nx][ny] > 1:
-                        check_brick(nx, ny)
-                    elif bricks[nx][ny] == 1:
-                        to_be_destroyed[nx][ny] = True
-                    elif bricks[nx][ny] == 0:
-                        break
+                if 0<=nx<h and 0<=ny<w and bricks[nx][ny]:
+                    q.append((nx, ny))
+    # 벽돌 정리
+    for row in range(w):
+        for col in range(h):
+            if bricks[row][col] == 0:
+                bricks[row] = bricks[row].pop(col) + bricks[row]
+
+def dfs(shot, last, col):
+    global w, h, min_bricks
+    if shot == last:
+        # 벽돌 복사
+        bricks = [[0]*h for _ in range(w)]
+        for col in range(w):
+            for row in range(h):
+                bricks[row][col] = orig[row][col]
+        shoot(bricks)
+        # 남은 벽돌 세기
+        rest = 0
+        for line in bricks:
+            rest += w - line.count(0)
+        if min_bricks > rest:
+            min_bricks = rest
+    else:
+        dfs(shot+1, last, col+1)
+        # 벽돌 깨기
 
 t = int(input())
 for test_case in range(1, t+1):
     n, h, w = map(int, input().split()) # swap w, h to use zip()
     data = [list(map(int, input().split())) for _ in range(w)]
-    bricks = [list(line) for line in list(zip(*data))]
-    
-    top = list()
-    for row in range(h):
-        for col in range(w):
-            if bricks[row][col]:
-                top.append((row, col))
-                break
-    x, y = top.pop()
-    to_be_destroyed = [[False for _ in range(w)] for _ in range(h)]
-    check_brick(x, y)
-    # check된 벽돌 없애고
-    # 없앤 자리 채우기
+    orig = [list(line) for line in list(zip(*data))]
+    # 경우의 수: w ** n
+    min_bricks = h * w
+    top = [0] * n
+    dfs(0, n, 0)
     print('#{}'.format(test_case))
