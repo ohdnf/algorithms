38e6c0c3efd618cedca7f95ab473b6efd46a40e4
ohdnf <jupyohong7@gmail.com>
2020-03-22 23:57:58 +0900

    Update functions

--- swea/mock_exam/5656/5656_brick_breaking.py
+++ swea/mock_exam/5656/5656_brick_breaking.py
@@ -4,25 +4,35 @@ sys.stdin = open('input.txt')
 def arrange():
     return
 
-def shoot(bricks):
+def shoot(shots, bricks):
+    global w, h
     q = list()
-    while q:
-        x, y = q.pop(0)
-        number = bricks[x][y]
-        bricks[x][y] = 0
-        for dx, dy in ((0, 1), (0, -1), (1, 0), (-1, 0)):
-            for dist in range(1, number):
-                nx = x + dx*dist
-                ny = y + dy*dist
-                if 0<=nx<h and 0<=ny<w and bricks[nx][ny]:
-                    q.append((nx, ny))
-    # 벽돌 정리
-    for row in range(w):
-        for col in range(h):
-            if bricks[row][col] == 0:
-                bricks[row] = bricks[row].pop(col) + bricks[row]
+    for col in top:
+        # 맨 위 벽돌 선택
+        for row in range(h):
+            if bricks[row][col]:
+                q.append((row, col))
+                break
+        else:
+            return False
+        # ㄹㅇ 벽돌 깨기
+        while q:
+            x, y = q.pop(0)
+            number = bricks[x][y]
+            bricks[x][y] = 0
+            for dx, dy in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+                for dist in range(1, number):
+                    nx = x + dx*dist
+                    ny = y + dy*dist
+                    if 0<=nx<h and 0<=ny<w and bricks[nx][ny]:
+                        q.append((nx, ny))
+        # 벽돌 정리
+        for row in range(w):
+            for col in range(h):
+                if bricks[row][col] == 0:
+                    bricks[row] = bricks[row].pop(col) + bricks[row]
 
-def dfs(shot, last, col):
+def dfs(shot, last):
     global w, h, min_bricks
     if shot == last:
         # 벽돌 복사
@@ -30,7 +40,8 @@ def dfs(shot, last, col):
         for col in range(w):
             for row in range(h):
                 bricks[row][col] = orig[row][col]
-        shoot(bricks)
+        # 벽돌 깨기
+        shoot(last, bricks)
         # 남은 벽돌 세기
         rest = 0
         for line in bricks:
@@ -38,16 +49,16 @@ def dfs(shot, last, col):
         if min_bricks > rest:
             min_bricks = rest
     else:
-        dfs(shot+1, last, col+1)
-        # 벽돌 깨기
+        for col in range(w):
+            top[shot] = col
+            dfs(shot+1, last)
 
 t = int(input())
 for test_case in range(1, t+1):
     n, h, w = map(int, input().split()) # swap w, h to use zip()
     data = [list(map(int, input().split())) for _ in range(w)]
     orig = [list(line) for line in list(zip(*data))]
-    # 경우의 수: w ** n
     min_bricks = h * w
     top = [0] * n
-    dfs(0, n, 0)
+    dfs(0, n)
     print('#{}'.format(test_case))
