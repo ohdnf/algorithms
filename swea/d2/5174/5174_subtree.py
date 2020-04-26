ea5f2da629eba5aab1164b734fcf99bb1145a2b8
ohdnf <jupyohong7@gmail.com>
2020-04-26 23:04:30 +0900

    Add SWEA 5174

--- swea/d2/5174/5174_subtree.py
+++ swea/d2/5174/5174_subtree.py
@@ -1,12 +1,21 @@
 import sys
 sys.stdin = open('input.txt')
 
-def dfs():
-    return
-
 t = int(input())
 for test_case in range(1, t+1):
     e, n = map(int, input().split())
-    p_c = list(map(int, input().split()))
+    edges = list(map(int, input().split()))
     adj = [list() for _ in range(e+2)]
+    for idx in range(0, e*2, 2):
+        parent = edges[idx]
+        child = edges[idx+1]
+        adj[parent].append(child)
+    q = [n,]
+    result = 0
+    while q:
+        curr = q.pop(0)
+        result += 1
+        for node in adj[curr]:
+            q.append(node)
+    print('#{} {}'.format(test_case, result))
     
