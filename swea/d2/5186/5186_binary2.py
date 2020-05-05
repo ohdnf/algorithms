1d6fcc5fa11eaf492fdcde9e7ead80e4726578f2
ohdnf <jupyohong7@gmail.com>
2020-05-05 22:55:07 +0900

    Update SWEA 5186

--- swea/d2/5186/5186_binary2.py
+++ swea/d2/5186/5186_binary2.py
@@ -4,4 +4,16 @@ sys.stdin = open('input.txt')
 t = int(input())
 for test_case in range(1, t+1):
     N = float(input())
-    print('#{} {}'.format(test_case, N))
+    result = ''
+    digits = 1
+    while N > 0 and digits <= 12:
+        curr = 2 ** -digits
+        digits += 1
+        if N - curr < 0:
+            result += '0'
+            continue
+        N -= curr
+        result += '1'
+    if digits > 12 and N > 0:
+        result = 'overflow'
+    print('#{} {}'.format(test_case, result))
