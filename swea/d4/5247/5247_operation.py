431234d8c8e4b3dc1b9e61800a3b3c566899f2bf
ohdnf <jupyohong7@gmail.com>
2020-06-25 17:09:03 +0900

    Add SWEA 5247: Counting operating trial using BFS

--- swea/d4/5247/5247_operation.py
+++ swea/d4/5247/5247_operation.py
@@ -1,29 +1,23 @@
 import sys
+from collections import deque
+
 sys.stdin = open('input.txt')
 
-able = ['+1', '-1', '*2', '-10']
-def dfs(number, target, trial):
-    global result
-    if number == target:
-        if result > trial:
-            result = trial
-    elif trial > result:
-        return
-    else:
-        for i in range(4):
-            if i == 0:
-                dfs(number*2, target, trial+1)
-            elif i == 1:
-                dfs(number-1, target, trial+1)
-            elif i == 2:
-                dfs(number+1, target, trial+1)
-            elif i == 3:
-                dfs(number-10, target, trial+1)
+def bfs(start, goal):
+    q = deque([start, ])
+    visited = [0] * 1000001
+    while q:
+        num = q.popleft()
+        for nxt in (num*2, num-1, num+1, num-10):
+            if 0 < nxt <= 1000000 and not visited[nxt]:
+                if nxt == goal:
+                    return visited[num] + 1
+                else:
+                    visited[nxt] = visited[num] + 1
+                    q.append(nxt)
 
 t = int(input())
 for test_case in range(1, t+1):
     N, M = map(int, input().split())
-    result = 1000000
-    dfs(N, M, 0)
-
-    print('#{} {}'.format(test_case, result))
+    trial = bfs(N, M)
+    print('#{} {}'.format(test_case, trial))
