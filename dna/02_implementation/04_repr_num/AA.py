5fef52816de07f68b430b7456ec19c5aa86a959d
ohdnf <jupyohong7@gmail.com>
2020-06-28 12:11:38 +0900

    Update dna: repr_num

--- dna/02_implementation/04_repr_num/AA.py
+++ dna/02_implementation/04_repr_num/AA.py
@@ -1,21 +1,21 @@
 import sys
 input = lambda: sys.stdin.readline()
-# sys.stdin = open('in1.txt')
-n = int(input())
-scores = list(map(int, input().split()))
-avg = sum(scores)//n
-students = [[abs(avg-score), score, idx+1] for idx, score in enumerate(scores)]
-students.sort(key=lambda s: s[0])
-result = list()
-min_value = students[0][0]
-for d, s, i in students:
-    if d == min_value:
-        result.append((s, i))
-    else:
-        break
-if len(result) <= 2:
-    result.sort(key=lambda s: -s[0])
-else:
-    result.sort(key=lambda s: s[1])
-score, number = result[0]
-print(score, number)
+for case in range(1, 6):
+    sys.stdin = open(f'in{case}.txt')
+    n = int(input())
+    scores = list(map(int, input().split()))
+    avg = int(sum(scores)/n+0.5)    # Python의 round() 함수는 round-half-even 방식을 취한다
+    min_diff = 101
+    min_score = 101
+    res_number = 101
+    for idx, score in enumerate(scores):
+        diff = abs(score - avg)
+        if diff < min_diff:
+            min_diff = diff
+            res_score = score
+            res_number = idx + 1
+        elif diff == min_diff:
+            if score > res_score:
+                res_score = score
+                res_number = idx + 1
+    print(avg, res_number)
