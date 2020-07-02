0b6f3f9efd5602f6ac2f8b0ab2342bc08ae0f03c
Jupyo Hong <jupyohong7@gmail.com>
2020-07-02 11:14:23 +0900

    Remove sys inputs

--- dna/02_implementation/04_repr_num/AA.py
+++ dna/02_implementation/04_repr_num/AA.py
@@ -1,21 +1,19 @@
 import sys
 input = lambda: sys.stdin.readline()
-for case in range(1, 6):
-    sys.stdin = open(f'in{case}.txt')
-    n = int(input())
-    scores = list(map(int, input().split()))
-    avg = int(sum(scores)/n+0.5)    # Python의 round() 함수는 round-half-even 방식을 취한다
-    min_diff = 101
-    min_score = 101
-    res_number = 101
-    for idx, score in enumerate(scores):
-        diff = abs(score - avg)
-        if diff < min_diff:
-            min_diff = diff
+n = int(input())
+scores = list(map(int, input().split()))
+avg = int(sum(scores)/n+0.5)    # Python의 round() 함수는 round-half-even 방식을 취한다
+min_diff = 101
+min_score = 101
+res_number = 101
+for idx, score in enumerate(scores):
+    diff = abs(score - avg)
+    if diff < min_diff:
+        min_diff = diff
+        res_score = score
+        res_number = idx + 1
+    elif diff == min_diff:
+        if score > res_score:
             res_score = score
             res_number = idx + 1
-        elif diff == min_diff:
-            if score > res_score:
-                res_score = score
-                res_number = idx + 1
-    print(avg, res_number)
+print(avg, res_number)
