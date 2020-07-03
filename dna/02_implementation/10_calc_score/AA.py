5ae2f1df218919e4510f293b79741340e563a352
Jupyo Hong <jupyohong7@gmail.com>
2020-07-03 19:23:19 +0900

    Add lecture answer

--- dna/02_implementation/10_calc_score/AA.py
+++ dna/02_implementation/10_calc_score/AA.py
@@ -4,22 +4,30 @@ input = lambda: sys.stdin.readline()
 # sys.stdin = open('in2.txt')
 
 n = int(input())
-check = list(map(int, input().split()))
-score = [0] * n
+answers = list(map(int, input().split()))
+# score = [0] * n
 
-before = False
-bonus = 0
-for i in range(n):
-    if check[i]:
-        if before:
-            score[i] = 1 + bonus
-            bonus += 1
-        else:
-            score[i] = 1
-            before = True
-            bonus = 1
+# before = False
+# bonus = 0
+# for i in range(n):
+#     if answers[i]:
+#         if before:
+#             score[i] = 1 + bonus
+#             bonus += 1
+#         else:
+#             score[i] = 1
+#             before = True
+#             bonus = 1
+#     else:
+#         before = False
+#         bonus = 0
+# print(sum(score))
+
+res = cnt = 0
+for answer in answers:
+    if answer == 1:
+        cnt += 1
+        res += cnt
     else:
-        before = False
-        bonus = 0
-
-print(sum(score))
+        cnt = 0
+print(res)
