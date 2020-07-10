b5570ba9b1ca8458fe12b13a179ac9a25b680819
ohdnf <jupyohong7@gmail.com>
2020-07-10 10:12:22 +0900

    Update while conditional statement solution

--- dna/04_binary_search_and_greedy/01_binary_search/AA.py
+++ dna/04_binary_search_and_greedy/01_binary_search/AA.py
@@ -13,5 +13,17 @@ def binary_search(num, l, r):
     elif numbers[c] < num:
         return binary_search(num, c, r)
 
-res = binary_search(m, 0, n-1)
-print(res + 1)
+# res = binary_search(m, 0, n-1)
+# print(res + 1)
+
+# Other solve
+lt = 0
+rt = n-1
+while lt <= rt:
+    mid = (lt + rt) // 2
+    if numbers[mid] == m:
+        print(mid+1)
+    elif numbers[mid] > m:
+        rt = mid - 1
+    elif numbers[mid] < m:
+        lt = mid + 1
