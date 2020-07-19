f5e00b5cb8215e40b3604c799600ad037ac8bb4c
Jupyo Hong <jupyohong7@gmail.com>
2020-07-19 21:09:18 +0900

    Update metal stick: remove duplicated codes

--- dna/05_data_structure/02_metal_stick/AA.py
+++ dna/05_data_structure/02_metal_stick/AA.py
@@ -8,13 +8,11 @@ result = 0
 for curr in arr:
     if curr == '(':
         stack.append(curr)
-        pre = curr
     else:
+        stack.pop()
         if pre == '(':
-            stack.pop() # 레이저 괄호 제거
             result += len(stack)
         else:
-            stack.pop()
             result += 1
-        pre = ')'
+    pre = curr
 print(result)
