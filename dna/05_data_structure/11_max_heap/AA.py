cb49776906ec08a919b783b3913d57c4b65cd757
Jupyo Hong <jupyohong7@gmail.com>
2020-07-21 11:22:32 +0900

    Add dna: max heap

---
+++ dna/05_data_structure/11_max_heap/AA.py
@@ -0,0 +1,15 @@
+import sys, heapq
+input = lambda: sys.stdin.readline()
+
+h = list()
+while True:
+    n = int(input())
+    if n > 0:
+        heapq.heappush(h, -n)
+    elif n == 0:
+        if h:
+            print(-heapq.heappop(h))
+        else:
+            print(-1)
+    elif n == -1:
+        break
