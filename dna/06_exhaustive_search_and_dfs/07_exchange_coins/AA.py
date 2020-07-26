244754e54b4730a413de1040badad6ce97b2a19b
Jupyo Hong <jupyohong7@gmail.com>
2020-07-27 00:17:26 +0900

    Reduce code lines

--- dna/06_exhaustive_search_and_dfs/07_exchange_coins/AA.py
+++ dna/06_exhaustive_search_and_dfs/07_exchange_coins/AA.py
@@ -13,19 +13,34 @@ m = int(input())
 #     change = m // coin
 #     cnt += change
 #     m -= change * coin
+# print(cnt)
 
+# 1차 코드: 통과
+# res = 500
+# def dfs(idx, remain, cnt):
+#     global res
+#     if idx == n:
+#         if remain == 0 and res > cnt:
+#             res = cnt
+#     elif cnt >= res:
+#         return
+#     else:
+#         curr = coins[idx]
+#         for c in range(remain//curr, -1, -1):
+#             dfs(idx+1, remain-curr*c, cnt+c)
+# dfs(0, m, 0)
+# print(res)
+
+# 2차 코드
 res = 500
-def dfs(idx, remain, cnt):
+def dfs(cnt, change):
     global res
-    if idx == n:
-        if remain == 0 and res > cnt:
-            res = cnt
-    elif cnt >= res:
+    if change == m and res > cnt:
+        res = cnt
+    elif change > m or res <= cnt:
         return
     else:
-        curr = coins[idx]
-        for c in range(remain//curr, -1, -1):
-            dfs(idx+1, remain-curr*c, cnt+c)
-dfs(0, m, 0)
-
+        for coin in coins:
+            dfs(cnt+1, change+coin)
+dfs(0, 0)
 print(res)
