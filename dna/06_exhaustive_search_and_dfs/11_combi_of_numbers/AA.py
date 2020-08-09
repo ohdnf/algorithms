e53857682b92ef8580ace20cb803e5bd564c7bb3
Jupyo Hong <jupyohong7@gmail.com>
2020-08-09 22:33:29 +0900

    Add dna: combi of numbers

--- dna/06_exhaustive_search_and_dfs/11_combi_of_numbers/AA.py
+++ dna/06_exhaustive_search_and_dfs/11_combi_of_numbers/AA.py
@@ -1,11 +1,32 @@
 import sys
-import itertools as it
-#sys.stdin=open("input.txt", "r")
-n, k=map(int, input().split())
-a=list(map(int, input().split()))
-m=int(input())
-cnt=0
-for x in it.combinations(a, k):
-    if sum(x)%m==0:
-        cnt+=1
-print(cnt)
+input = lambda: sys.stdin.readline()
+
+n, k = map(int, input().split())
+nums = list(map(int, input().split()))
+m = int(input())
+
+tmp = [0] * k
+chk = {num: False for num in nums}
+res = 0
+
+def dfs(level, total, start):
+# def dfs(level, start):
+    global res
+    if level == k:
+        if total % m == 0:
+            res += 1
+        # if sum(tmp) % m == 0:
+        #     res += 1
+    else:
+        for idx in range(start, n):
+            if not chk[nums[idx]]:
+                chk[nums[idx]] = True
+                tmp[level] = nums[idx]
+                dfs(level+1, total+nums[idx], idx+1)
+                # dfs(level+1, idx+1)
+                chk[nums[idx]] = False
+
+dfs(0, 0, 0)
+# dfs(0, 0)
+
+print(res)
