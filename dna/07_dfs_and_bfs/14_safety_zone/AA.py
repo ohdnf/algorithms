ad1993f8357364cfdd60157f1b417f9fe55d0b38
Jupyo Hong <jupyohong7@gmail.com>
2020-08-17 14:55:26 +0900

    Add dna: find max safe area

--- dna/07_dfs_and_bfs/14_safety_zone/AA.py
+++ dna/07_dfs_and_bfs/14_safety_zone/AA.py
@@ -1,36 +1,47 @@
+from collections import deque as dq
 import sys
-#sys.stdin=open("input.txt", "r")
-dx=[-1, 0, 1, 0]
-dy=[0, 1, 0, -1]
-sys.setrecursionlimit(10**6)
-def DFS(x, y, h):
-    ch[x][y]=1
-    for i in range(4):
-        xx=x+dx[i]
-        yy=y+dy[i]
-        if 0<=xx<n and 0<=yy<n and ch[xx][yy]==0 and board[xx][yy]>h:
-            DFS(xx, yy, h)
+# sys.stdin = open("in1.txt")
 
-if __name__=="__main__":
-    n = int(input())
-    cnt = 0
-    res = 0
-    board=[list(map(int, input().split())) for _ in range(n)]
-    for h in range(100):
-        ch=[[0]*n for _ in range(n)]
-        cnt=0
-        for i in range(n):
-            for j in range(n):
-                if ch[i][j]==0 and board[i][j]>h:
-                    cnt+=1
-                    DFS(i, j, h)
-        res=max(res, cnt)
-        if cnt==0:
-            break
-    print(res)
+d = ((0,1),(1,0),(0,-1),(-1,0))
 
+n = int(input())
+area = list()
+precipitations = set()
+for _ in range(n):
+    line = list(map(int, input().split()))
+    area.append(line)
+    for h in line:
+        precipitations.add(h)
 
+max_safe_area = 0
 
-
-
+# 모든 강수량에 대해 안전 지역 개수 검사
+for p in precipitations:
+    # 침수 지역 초기화
+    flooded = [[False]*n for _ in range(n)]
+    # 침수 지역 표시
+    for row in range(n):
+        for col in range(n):
+            if area[row][col] <= p:
+                flooded[row][col] = True
+    safe_area = 0
+    # 안전 지역 검사
+    for row in range(n):
+        for col in range(n):
+            if not flooded[row][col]:
+                search = dq()
+                search.append((row, col))
+                flooded[row][col] = True    # 안전지역 확인된 곳은 True로 변경
+                while search:
+                    crow, ccol = search.popleft()
+                    for drow, dcol in d:
+                        nrow, ncol = crow+drow, ccol+dcol
+                        if 0<=nrow<n and 0<=ncol<n and not flooded[nrow][ncol]:
+                            flooded[nrow][ncol] = True
+                            search.append((nrow, ncol))
+                safe_area += 1
     
+    if max_safe_area < safe_area:
+        max_safe_area = safe_area
+
+print(max_safe_area)
