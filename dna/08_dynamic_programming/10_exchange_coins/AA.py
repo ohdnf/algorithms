4e859d46410fb2cb0cb204eea1abe9bbfe0a2dfb
Jupyo Hong <jupyohong7@gmail.com>
2020-08-28 14:42:59 +0900

    Update exchange coins

--- dna/08_dynamic_programming/10_exchange_coins/AA.py
+++ dna/08_dynamic_programming/10_exchange_coins/AA.py
@@ -7,8 +7,8 @@ m = int(input())    # 거슬러 줄 금액
 
 dp = [0] + [501] * m    # x원을 거슬러 주는 데 사용된 동전의 최소 개수(0원은 0개로 초기화)
 
-for i in range(n):
-    for j in range(coin[i], m+1):
-        dp[j] = min(dp[j], dp[j-coin[i]]+1)
+for c in coin:
+    for j in range(c, m+1):
+        dp[j] = min(dp[j], dp[j-c]+1)
 
 print(dp[m])
