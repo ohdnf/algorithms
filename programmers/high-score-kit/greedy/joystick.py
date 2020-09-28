143a3c9c0ce26f87e9d876e44b265183a3a1ebff
Jupyo Hong <jupyohong7@gmail.com>
2020-09-28 16:30:15 +0900

    조이스틱(그리디)

--- programmers/high-score-kit/greedy/joystick.py
+++ programmers/high-score-kit/greedy/joystick.py
@@ -46,3 +46,15 @@ if __name__ == "__main__":
     print(solution("JAN"), 23)
     print(solution("BBBAAAB"), 9)
     print(solution("ABABAAAAABA"), 11)
+    print(solution("ABAAABAAAAAAB"), 12)
+    print(solution("BABAAAAAAAAAB"), 8)
+
+
+"""
+로직
+1. 각 자리의 알파벳 ord값에서 ord("A")값을 감산
+2. 현재 커서 위치의 알파벳 조작
+3. 왼쪽과 오른쪽 중 최소 이동으로 커서 위치 변경
+(이 때, 조건에서 커서가 맨 오른쪽에서 이동할 경우 첫 번째 문자로 갈 수 있다고 명시되지 않았으므로
+왼쪽과 오른쪽이 같을 경우엔 왼쪽으로 오른쪽으로 )
+"""
