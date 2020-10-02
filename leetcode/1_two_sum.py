97f9d45b27ead3a19dbe6a01047a5fabfad4bc8f
Jupyo Hong <jupyohong7@gmail.com>
2020-10-03 01:34:08 +0900

    first problem of leetcode

---
+++ leetcode/1_two_sum.py
@@ -0,0 +1,24 @@
+from typing import List
+
+# brute-force
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+# one-pass hash table
+class Solution:
+    def two_sum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, v in enumerate(nums):
+            remaining = target - v
+            if remaining in seen:
+                return [seen[remaining], i]
+            seen[v] = i
+        return []
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.twoSum([-1,-2,-3,-4,-5], -8), [2, 4])
