e23917b66773377d442e5c90f1ca12afd5cbe92d
Jupyo Hong <jupyohong7@gmail.com>
2020-10-12 00:39:36 +0900

    Add two pointer description

--- leetcode/15_three_sum.py
+++ leetcode/15_three_sum.py
@@ -21,6 +21,7 @@ class Solution:
                         triplets.append([nums[i], nums[j], nums[k]])
         return triplets
 
+    # Two Pointers
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         triplets = []
         nums.sort()
@@ -55,3 +56,10 @@ if __name__ == "__main__":
     print(solution.threeSum([]), [])
     print(solution.threeSum([13,-4,1,3,-1,-1,5,-11,13,9,4,7,5,-5,-13,-4,8,-3,14,-4,14,6,7,11,4,-6,-5,-9,14,3,-9,12,-15,0,-8,-9,14,4,-5,4,-1,-15,-12,-11,-13,-9,1,3,-5,0,14,-6,13,-1,12,2,8,-7,9,0,11,7,-11,3,-8,-11,1,13,8,4,-5,14,4,-2,11,-2,-4,-3,-14,6,4,8,7,3,-8,5,12,7,5,-2,-8,-7,13,-11,12,12,-7,-10,11,-14]), [])
     print(solution.threeSum([0]), [])
+
+
+"""
+투 포인터(Two pointers) 기법
+- 시작점(왼쪽)과 끝점(오른쪽), 두 지점을 기준으로 범위를 좁혀나가는 문제 풀이 전략
+- 주로 정렬된 배열에서 사용
+"""
