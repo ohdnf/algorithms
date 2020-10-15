97e9a849467317a31b416937251a2d4247e6db7f
Jupyo Hong <jupyohong7@gmail.com>
2020-10-15 19:26:15 +0900

    Add other solution

--- leetcode/2_add_two_numbers.py
+++ leetcode/2_add_two_numbers.py
@@ -1,6 +1,9 @@
+from typing import List
+from functools import reduce
+from operator import add
+
+
 # Definition for singly-linked list.
-
-
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -8,6 +11,7 @@ class ListNode:
 
 
 class Solution:
+    # 전가산기 구현
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         carry = 0
         result = curr = ListNode()
@@ -25,3 +29,35 @@ class Solution:
             curr = curr.next
 
         return result.next
+
+    # 자료형 변환
+    def reverseList(self, head: ListNode) -> ListNode:
+        node, prev = head, None
+
+        while node:
+            next, node.next = node.next, prev
+            prev, node = node, next
+
+        return prev
+
+    def toList(self, node: ListNode) -> List:
+        list: List = []
+        while node:
+            list.append(node.val)
+            node = node.next
+        return list
+
+    def toReversedLinkedList(self, result: str) -> ListNode:
+        prev: ListNode = None
+        for r in result:
+            node = ListNode(r)
+            node.next = prev
+            prev = node
+        return node
+
+    def addTwoNumbers2(self, l1: ListNode, l2: ListNode) -> ListNode:
+        a = self.toList(self.reverseList(l1))
+        b = self.toList(self.reverseList(l2))
+        # resultStr = int(''.join(map(str, a))) + int(''.join(map(str, b)))
+        resultStr = reduce(lambda x, y: 10 * x + y, a, 0) + reduce(lambda x, y: 10 * x + y, b, 0)
+        return self.toReversedLinkedList(str(resultStr))
