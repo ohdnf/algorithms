deef899541f178cebd8db4b6d6b4672cc84de6df
Jupyo Hong <jupyohong7@gmail.com>
2020-10-26 23:54:35 +0900

    Add headq solution

--- leetcode/23_merge_k_sorted_lists.py
+++ leetcode/23_merge_k_sorted_lists.py
@@ -1,4 +1,5 @@
 from typing import List
+import heapq
 
 
 # Definition for singly-linked list.
@@ -21,3 +22,22 @@ class Solution:
             head.next = ListNode(num)
             head = head.next
         return merged_list.next
+
+    def mergeKLists_heapq(self, lists: List[ListNode]) -> ListNode:
+        root = curr = ListNode()
+        heap = []
+
+        for idx, head in enumerate(lists):
+            if head:
+                heapq.heappush(heap, (head.val, idx, head))
+
+        while heap:
+            node = heapq.heappop(heap)
+            idx = node[1]
+            curr.next = node[2]
+
+            curr = curr.next
+            if curr.next:
+                heapq.heappush(heap, (curr.next.val, idx, curr.next))
+
+        return root.next
