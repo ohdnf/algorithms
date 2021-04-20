0bafb0e5badb3b7a64a75cb637e0b902d6527f33
Jupyo Hong <jupyohong7@gmail.com>
2021-04-21 00:46:57 +0900

    Update 1242_소풍.py
    제거된 사람 다음을 1번부터 다시 시작

--- acmicpc/1242_소풍.py
+++ acmicpc/1242_소풍.py
@@ -1,21 +1,33 @@
 n, k, m = map(int, input().split())
 
+# 방법 1
 # 나머지 연산을 위해 인덱스를 1...n에서 0...(n-1)로 변경
-m -= 1
-start = 0
-out = 0
+# m -= 1
+# start = 0
+# out = 0
 
-while True:
-    next_out = (start + k - 1) % n
-    out += 1
-    if next_out == m:
-        break
-    elif next_out < m:
-        m -= 1
-    n -= 1
-    start = next_out
+# while True:
+#     next_out = (start + k - 1) % n
+#     out += 1
+#     if next_out == m:
+#         break
+#     elif next_out < m:
+#         m -= 1
+#     n -= 1
+#     start = next_out
 
-print(out)
+# print(out)
 
 
+# 방법 2
 # 사람이 탈락할 때마다 다음 사람부터 번호 1로 다시 시작
+out = 0
+while True:
+    out += 1
+    m -= k
+    while m < 0:
+        m += n
+    if m == 0:
+        break
+    n -= 1
+print(out)
